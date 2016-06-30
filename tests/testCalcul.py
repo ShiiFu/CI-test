76f926fdd3ca30b458fe64d77e23c1d6e4bd8e1b
shiifu <damien7leroy@gmail.com>
2016-06-30 09:35:08 +0000

    Added some tests

--- tests/testCalcul.py
+++ tests/testCalcul.py
@@ -1,12 +1,19 @@
 import unittest
-from CI-test.calcul.addition import *
+from calcul.addition import *
 
 class TestCalcul(unittest.TestCase) :
  
-	def test_calcul(self) :
+	def test_calcul1(self) :
 		resultat = addition(5, 2)
 		self.assertEqual(resultat, 7)
-        
+ 
+	def test_calcul2(self) :
+		resultat = addition(17, 18)
+		self.assertEqual(resultat, 35)
+ 
+	def test_calcul3(self) :
+		resultat = addition(2555, 445)
+		self.assertEqual(resultat, 3000)
 
 if __name__ == '__main__':
 	unittest.main()
